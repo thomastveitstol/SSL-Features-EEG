e3a4cd3c41ff4e0a6803f4940bd3adf3123f1c37
thomas <thomas.tveitstol@gmail.com>
2024-08-16 15:52:03 +0200

    fixed bash script

--- scripts/data_preparation.bash
+++ scripts/data_preparation.bash
@@ -1,5 +1,5 @@
-python3 -m srm
-python3 -m miltiadous
-python3 -m lemon
-python3 -m wang
-python3 -m band_power
+python3 -m dataset_downloads.srm
+python3 -m dataset_downloads.miltiadous
+python3 -m dataset_downloads.lemon
+python3 -m dataset_downloads.wang
+python3 -m compute_pseudo_targets.band_power
