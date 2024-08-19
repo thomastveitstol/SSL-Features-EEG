a3b20b55781e1c95eb92a919ae097fab4733813d
thomas <thomas.tveitstol@gmail.com>
2024-08-19 16:58:43 +0200

    updates for both ocular states

--- scripts/prepare_input_data/band_pass.py
+++ scripts/prepare_input_data/band_pass.py
@@ -8,13 +8,7 @@ from elecssl.data.datasets.dataset_base import DatasetInfo, OcularState
 from elecssl.data.datasets.getter import get_dataset
 
 
-def main():
-    # ---------------
-    # Read config file
-    # ---------------
-    with open(os.path.join(os.path.dirname(__file__), "config_files", "band_pass.yml")) as file:
-        config: Dict[str, Any] = yaml.safe_load(file)
-
+def _single_ocular_state(config):
     # ---------------
     # Prepare info
     # ---------------
@@ -39,6 +33,27 @@ def main():
     # Save the transformed input data
     # ---------------
     BandPass().save_data(datasets=tuple(datasets), config=config, save_data=True, plot_data=False)
+
+
+def main():
+    # ---------------
+    # Read config file
+    # ---------------
+    with open(os.path.join(os.path.dirname(__file__), "config_files", "band_pass.yml")) as file:
+        config: Dict[str, Any] = yaml.safe_load(file)
+
+    # ---------------
+    # Loop through the ocular states
+    # ---------------
+    for ocular_state, dataset_config in config["OcularStates"].items():
+        # Fix config file for current ocular state
+        ocular_state_config = config.copy()
+        ocular_state_config["OcularState"] = ocular_state
+        ocular_state_config["Datasets"] = dataset_config["Datasets"]
+        del ocular_state_config["OcularStates"]
+
+        # Create features for current ocular state
+        _single_ocular_state(ocular_state_config)
 
 
 if __name__ == "__main__":
