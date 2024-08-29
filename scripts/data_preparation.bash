37cc7292c9a00a8f327c6894d06e34f07005f11f
thomas <thomas.tveitstol@gmail.com>
2024-08-29 13:32:05 +0200

    commented out code

--- scripts/data_preparation.bash
+++ scripts/data_preparation.bash
@@ -1,6 +1,6 @@
-python3 -m dataset_downloads.srm
-python3 -m dataset_downloads.miltiadous
-python3 -m dataset_downloads.lemon
-python3 -m dataset_downloads.wang
+# python3 -m dataset_downloads.srm
+# python3 -m dataset_downloads.miltiadous
+# python3 -m dataset_downloads.lemon
+# python3 -m dataset_downloads.wang
 python3 -m compute_pseudo_targets.band_power
 python3 -m prepare_input_data.band_pass
