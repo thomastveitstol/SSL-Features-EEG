b6a9a88212ddf76e6183a70feef9bfc8c79a2084
thomas <thomas.tveitstol@gmail.com>
2024-08-30 12:50:37 +0200

    changes to script

--- scripts/visualise_data/plot_eeg_data.py
+++ scripts/visualise_data/plot_eeg_data.py
@@ -1,3 +1,9 @@
+"""
+Script I made after notch filter failed for some subjects in the LEMON dataset. I was because the sampling frequency is
+not the same for all.
+
+However, it doesn't look like a notch filter is needed for LEMON
+"""
 from matplotlib import pyplot
 
 from elecssl.data.datasets.dataset_base import OcularState
@@ -6,12 +12,13 @@ from elecssl.data.datasets.lemon import LEMON
 
 def main():
     # Select subject and load data
-    sub_id = LEMON().get_subject_ids()[30]
+    sub_id = LEMON().get_subject_ids()[167]
     eeg = LEMON().load_single_mne_object(subject_id=sub_id, ocular_state=OcularState.EC, interpolation_method="MNE")
 
     # Apply notch filter
     print(eeg.info["sfreq"])
     # eeg.notch_filter(50)
+    eeg.resample(250)
 
     # Plot
     eeg.plot()
