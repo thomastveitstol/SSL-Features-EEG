bdd641e266c10fbb6bae2c6016abc81153d2e770
thomas <thomas.tveitstol@gmail.com>
2024-10-01 10:09:11 +0200

    improved skipping condition

--- tests/data/test_paths.py
+++ tests/data/test_paths.py
@@ -6,26 +6,28 @@ from elecssl.data.paths import (get_results_dir, get_raw_data_storage_path, get_
                                 get_eeg_features_storage_path)
 
 
-@pytest.mark.skipif("GITHUB_ACTIONS" in os.environ, reason="Github does not have the required files")
+_IS_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
+
+
+@pytest.mark.skipif(_IS_GITHUB_ACTIONS, reason="Github does not have the required files")
 def test_get_results_dir():
     """Test if results path exists"""
-    assert False, os.environ
     assert os.path.isdir(get_results_dir())
 
 
-@pytest.mark.skipif("GITHUB_ACTIONS" in os.environ, reason="Github does not have the required files")
+@pytest.mark.skipif(_IS_GITHUB_ACTIONS, reason="Github does not have the required files")
 def test_get_raw_data_storage_path():
     """Test if path where raw data is expected to be exists"""
     assert os.path.isdir(get_raw_data_storage_path())
 
 
-@pytest.mark.skipif("GITHUB_ACTIONS" in os.environ, reason="Github does not have the required files")
+@pytest.mark.skipif(_IS_GITHUB_ACTIONS, reason="Github does not have the required files")
 def test_get_numpy_data_storage_path():
     """Test if path where numpy arrays (input data) are expected to be exists"""
     assert os.path.isdir(get_numpy_data_storage_path())
 
 
-@pytest.mark.skipif("GITHUB_ACTIONS" in os.environ, reason="Github does not have the required files")
+@pytest.mark.skipif(_IS_GITHUB_ACTIONS, reason="Github does not have the required files")
 def test_get_eeg_features_storage_path():
     """Test if path where features for SSL are expected to be exists"""
     assert os.path.isdir(get_eeg_features_storage_path())
