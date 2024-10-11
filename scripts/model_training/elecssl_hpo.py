78ea59c545d73f651e54b8b6fa4dc3ccc4778d1f
thomas <thomas.tveitstol@gmail.com>
2024-10-11 17:13:58 +0200

    fixed results dir generation and running of HPO experiments

--- scripts/model_training/elecssl_hpo.py
+++ scripts/model_training/elecssl_hpo.py
@@ -1,4 +1,5 @@
 import os
+from datetime import date, datetime
 from pathlib import Path
 
 from elecssl.data.paths import get_results_dir
@@ -11,14 +12,14 @@ def main():
     # ----------------
     hp_config_path = Path(os.path.dirname(__file__)) / "config_files" / "hp_distributions"
     experiments_config_path = Path(os.path.dirname(__file__)) / "config_files" / "configurations"
-    results_dir = get_results_dir()
+    results_dir = get_results_dir() / f"hpo_experiment_{date.today()}_{datetime.now().strftime('%H%M%S')}"
 
     # ----------------
     # Run experiments
     # ----------------
     HPOExperiment(
         hp_config_path=hp_config_path, experiments_config_path=experiments_config_path, results_dir=results_dir
-    )
+    ).run_hyperparameter_optimisation()
 
 
 if __name__ == "__main__":
