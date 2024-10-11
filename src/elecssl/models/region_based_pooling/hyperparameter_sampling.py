9aa916424b4c1a49c6adbbe4578989758dea681b
thomas <thomas.tveitstol@gmail.com>
2024-10-11 09:22:20 +0200

    changed function from private to public

--- src/elecssl/models/region_based_pooling/hyperparameter_sampling.py
+++ src/elecssl/models/region_based_pooling/hyperparameter_sampling.py
@@ -2,7 +2,7 @@ import random
 from typing import Any, Dict
 
 
-def _generate_partition_sizes(*, n, k):
+def generate_partition_sizes(*, n, k):
     """
     Function for randomly assigning cardinalities to subsets of a set of length n to partition. Any solution in positive
     integers to the of the equation x_1 + x_2 + ... + x_k = n is ok.
@@ -21,12 +21,12 @@ def _generate_partition_sizes(*, n, k):
     Examples
     --------
     >>> random.seed(2)
-    >>> _generate_partition_sizes(n=10, k=3)
+    >>> generate_partition_sizes(n=10, k=3)
     (5, 2, 3)
 
     The sum will always equal n
 
-    >>> all(sum(_generate_partition_sizes(n=n_, k=k_)) == n_  # type: ignore[attr-defined]
+    >>> all(sum(generate_partition_sizes(n=n_, k=k_)) == n_  # type: ignore[attr-defined]
     ...         for n_, k_ in zip((10, 20, 15, 64), (5, 10, 5, 33)))
     True
     """
@@ -47,7 +47,7 @@ def _generate_partition_sizes(*, n, k):
 # ------------
 def yaml_generate_partition_sizes(loader, node):
     n, k = loader.construct_sequence(node)
-    return _generate_partition_sizes(n=n, k=k)
+    return generate_partition_sizes(n=n, k=k)
 
 
 def _traverse_and_call(obj):
