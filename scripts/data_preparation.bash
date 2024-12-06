4bc96a4b0aae5ef0358370155cb8ee40644173bb
thomas <thomas.tveitstol@gmail.com>
2024-12-06 17:03:13 +0100

    minor changes

--- scripts/data_preparation.bash
+++ scripts/data_preparation.bash
@@ -2,5 +2,5 @@
 # python3 -m dataset_downloads.miltiadous
 # python3 -m dataset_downloads.lemon
 # python3 -m dataset_downloads.wang
-python3 -m compute_pseudo_targets.band_power
 python3 -m prepare_input_data.band_pass
+python3 -m compute_pseudo_targets.band_power
