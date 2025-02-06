356e6d9e5e48f2403eec14c95e1d9274d158d3e7
thomas <thomas.tveitstol@gmail.com>
2025-02-06 10:57:05 +0100

    improvements to loading main torch model

--- src/elecssl/models/main_models/main_base_class.py
+++ src/elecssl/models/main_models/main_base_class.py
@@ -11,8 +11,8 @@ class MainModuleBase(nn.Module):
     """
 
     @classmethod
-    def load_model(cls, path: Path) -> "MainModuleBase":
-        model = torch.load(path)
+    def load_model(cls, name: str, path: Path) -> "MainModuleBase":
+        model = torch.load((path / name).with_suffix(".pt"))
         if not isinstance(model, cls):
             raise ModuleLoadError(f"Expected the loaded module to be from the same class as attempted loaded from "
                                   f"({cls.__name__}), but got {type(model)}")
