f3481468fb908f622cff040d6feb6d9ad52f6887
thomas <thomas.tveitstol@gmail.com>
2025-02-10 18:11:24 +0100

    added function for converting to 'Hyperparameter'

---
+++ src/elecssl/data/results_analysis/hyperparameters.py
@@ -0,0 +1,52 @@
+import dataclasses
+from typing import Callable, Tuple, Union, Any, Dict
+
+from ConfigSpace import CategoricalHyperparameter, UniformFloatHyperparameter
+from ConfigSpace.hyperparameters import Hyperparameter
+
+
+@dataclasses.dataclass(frozen=True)
+class HP:
+    # How to find the HPC in the HPC config file
+    hpc: Union[Tuple[str, ...], Callable[[Dict[str, Any]], Any]]
+    # How to find the HPD in the HPD config file
+    hpd: Callable[[Dict[str, Any]], Hyperparameter]
+
+
+# --------------
+# Individual HPs
+# --------------
+def to_hyperparameter(*, name, method, **kwargs):
+    if method == "categorical":
+        return CategoricalHyperparameter(name=name, **kwargs)
+    elif method == "float":
+        return UniformFloatHyperparameter(name=name, lower=kwargs["low"], upper=kwargs["high"], log=False)
+    raise ValueError(f"Sampling distribution of HP '{name}' not recognised: {method}")
+
+
+"""def get_normalisation_hpc(config):
+    if config["Varied Numbers of Channels"]["name"] == "Interpolation":
+        return config["DL Architecture"]["normalise"]
+    elif config["Varied Numbers of Channels"]["name"] == "RegionBasedPooling":
+        return config["Varied Numbers of Channels"]["kwargs"]["normalise_region_representations"]
+    else:
+        raise ValueError
+
+def get_normalisation_hpd(config):
+    return CategoricalHyperparameter(name="Normalisation", **config["normalisation"])
+
+def get_dl_architecture_hpd(config):
+    return CategoricalHyperparameter(name="DL architecture", choices=tuple(config["DLArchitectures"]))
+
+
+
+CHP = CategoricalHyperparameter
+OHP = OrdinalHyperparameter()
+UFHP = UniformFloatHyperparameter()
+HYPERPARAMETERS = MappingProxyType({
+    "Normalisation": HP(hpc=_get_normalisation_hpc, hpd=_get_normalisation_hpd),
+    "DL architecture": HP(("DLArchitecture", "model"), hpd=_get_dl_architecture_hpd),
+    "Learning rate": HP,
+    r"$\beta_1$": HP,
+    r"$\beta_2$": HP
+})"""
