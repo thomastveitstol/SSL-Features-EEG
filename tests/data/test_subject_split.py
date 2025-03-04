e962562180b0db777fb5bc15ac827e9bd50228a4
thomas <thomas.tveitstol@gmail.com>
2025-03-04 16:39:27 +0100

    added test

--- tests/data/test_subject_split.py
+++ tests/data/test_subject_split.py
@@ -2,9 +2,28 @@ import random
 
 import pytest
 
+from elecssl.data.subject_split import simple_random_split, RandomSplitsTVTestHoldout, Subject
 from tests.data.conftest import splits_and_kwargs
 
 
+# --------------
+# Functions
+# --------------
+def _get_shuffled_dataset(original_dataset_subjects):
+    shuffled_dataset_subjects = dict()
+    shuffled_dataset_names = list(original_dataset_subjects)
+    random.shuffle(shuffled_dataset_names)
+    for dataset_name in shuffled_dataset_names:
+        shuffled_subjects = list(original_dataset_subjects[dataset_name])
+        random.shuffle(shuffled_subjects)
+
+        shuffled_dataset_subjects[dataset_name] = tuple(shuffled_subjects)
+    return shuffled_dataset_subjects
+
+
+# --------------
+# Tests
+# --------------
 def test_non_bool_sort_error(splits_and_kwargs):
     """Test if passing 'sort_first' string as string correctly raises TypeError"""
     for subject_class, kwargs in splits_and_kwargs:
@@ -51,14 +70,7 @@ def test_order_invariance(splits_and_kwargs):
         splits = []
         for _ in range(10):
             # Shuffle
-            shuffled_dataset_subjects = dict()
-            shuffled_dataset_names = list(original_dataset_subjects)
-            random.shuffle(shuffled_dataset_names)
-            for dataset_name in shuffled_dataset_names:
-                shuffled_subjects = list(original_dataset_subjects[dataset_name])
-                random.shuffle(shuffled_subjects)
-
-                shuffled_dataset_subjects[dataset_name] = tuple(shuffled_subjects)
+            shuffled_dataset_subjects = _get_shuffled_dataset(original_dataset_subjects.copy())
 
             # Create split
             new_kwargs = kwargs.copy()
@@ -80,14 +92,7 @@ def test_order_non_invariance(splits_and_kwargs):
         splits = []
         for _ in range(10):
             # Shuffle
-            shuffled_dataset_subjects = dict()
-            shuffled_dataset_names = list(original_dataset_subjects)
-            random.shuffle(shuffled_dataset_names)
-            for dataset_name in shuffled_dataset_names:
-                shuffled_subjects = list(original_dataset_subjects[dataset_name])
-                random.shuffle(shuffled_subjects)
-
-                shuffled_dataset_subjects[dataset_name] = tuple(shuffled_subjects)
+            shuffled_dataset_subjects = _get_shuffled_dataset(original_dataset_subjects.copy())
 
             # Create split
             new_kwargs = kwargs.copy()
@@ -96,3 +101,34 @@ def test_order_non_invariance(splits_and_kwargs):
 
         # All splits should not be the same
         assert not all(split == splits[0] for split in splits)
+
+
+@pytest.mark.parametrize("func_kwargs,class_kwargs", [
+    ({"split_percent": 0.3, "seed": 2, "require_seeding": True, "sort_first": True},
+     {"val_split": 0.5, "seed": 2, "test_split": 0.3, "num_random_splits": 4, "sort_first": True}),
+    ({"split_percent": 0.37, "seed": 9, "require_seeding": True, "sort_first": True},
+     {"val_split": 0.1, "seed": 9, "test_split": 0.37, "num_random_splits": 1, "sort_first": True}),
+    ({"split_percent": 0.09, "seed": 7, "require_seeding": True, "sort_first": True},
+     {"val_split": 0.41, "seed": 7, "test_split": 0.09, "num_random_splits": 9, "sort_first": True})
+])
+def test_subject_split_equality(dummy_dataset_subjects, func_kwargs, class_kwargs):
+    """Test if 'simple_random_split' and 'RandomSplitsTVTestHoldout' produces the same test set when provided similar
+    input arguments. Only expected to work when 'sort_first' is True and the same seed is provided"""
+    for _ in range(10):
+        shuffled_subjects_1 = _get_shuffled_dataset(dummy_dataset_subjects.copy())
+        shuffled_subjects_2 = _get_shuffled_dataset(dummy_dataset_subjects.copy())
+
+        # Use the 'simple_random_split' and 'RandomSplitsTVTestHoldout'
+        flattened_subjects = [Subject(dataset_name=dataset_name, subject_id=sub_id)
+                              for dataset_name, sub_ids in shuffled_subjects_1.items() for sub_id in sub_ids]
+        random.shuffle(flattened_subjects)
+        test_set_1 = simple_random_split(subjects=flattened_subjects, **func_kwargs)[-1]
+
+        splits_2 = RandomSplitsTVTestHoldout(dataset_subjects=shuffled_subjects_2, **class_kwargs).splits
+        test_sets_2 = tuple(split[-1] for split in splits_2)
+        assert all(test_set == test_sets_2[0] for test_set in test_sets_2)
+        test_set_2 = test_sets_2[0]
+
+        # They should be the same
+        assert test_set_1 == test_set_2, ("The set sets are not the same when using 'RandomSplitsTVTestHoldout' and "
+                                          "'simple_random_split'")
