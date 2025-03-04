cbb16f19217e4d1bc2a591b60646b9778d798fba
thomas <thomas.tveitstol@gmail.com>
2025-03-04 16:42:31 +0100

    improved script

--- scripts/model_training/elecssl_method/continue_prediction_models_hpo.py
+++ scripts/model_training/elecssl_method/continue_prediction_models_hpo.py
@@ -6,10 +6,9 @@ def main():
     # ----------------
     # Continue experiment
     # ----------------
-    path = get_results_dir() / "prediction_models" / "debug_prediction_models_hpo_experiment_2025-02-28_155939"
-    prediction_models_experiment = PredictionModelsHPO.load_previous(path=path)
-    with prediction_models_experiment as experiment:
-        experiment.continue_hyperparameter_optimisation(path=path, num_trials=4)
+    path = get_results_dir() / "prediction_models" / "prediction_models_hpo_experiment_2025-03-04_012624"
+    with PredictionModelsHPO.load_previous(path=path) as experiment:
+        experiment.continue_hyperparameter_optimisation(path=path, num_trials=2)
 
 if __name__ == "__main__":
     main()
