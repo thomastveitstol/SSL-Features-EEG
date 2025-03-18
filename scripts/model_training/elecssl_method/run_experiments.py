e9f71f981de75df019a0872c273ef1328f9e0a7d
thomas <thomas.tveitstol@gmail.com>
2025-03-18 19:08:53 +0100

    updates after refactoring

--- scripts/model_training/elecssl_method/run_experiments.py
+++ scripts/model_training/elecssl_method/run_experiments.py
@@ -11,14 +11,10 @@ from elecssl.models.experiments.hpo_experiment import AllHPOExperiments
 def main():
     # Paths
     results_dir = get_results_dir()
-
-    config_folder = Path(os.path.dirname(__file__)) / "config_files"
-    static_folder = config_folder / "static_configurations"  # Non-HPO related configurations
-    hpd_folder = config_folder / "hyperparameter_distributions"  # HPO related configurations
+    config_path = Path(os.path.dirname(__file__)) / "config_files"
 
     # Run experiments
-    with AllHPOExperiments(results_dir=results_dir, hpd_configs_path=hpd_folder,
-                           experiments_configs_path=static_folder) as experiments:
+    with AllHPOExperiments(results_dir=results_dir, config_path=config_path) as experiments:
         experiments.run_experiments()
 
 
