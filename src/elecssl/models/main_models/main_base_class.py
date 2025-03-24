345565fb617d24e09cdc703bd14912609f2effca
thomas <thomas.tveitstol@gmail.com>
2025-03-24 15:45:26 +0100

    added abstract method

--- src/elecssl/models/main_models/main_base_class.py
+++ src/elecssl/models/main_models/main_base_class.py
@@ -1,15 +1,28 @@
+import abc
 from pathlib import Path
+from typing import Tuple
 
 import torch
 import torch.nn as nn
 
+from elecssl.models.metrics import Histories
 
-class MainModuleBase(nn.Module):
+
+class MainModuleBase(nn.Module, abc.ABC):
     """
     Base class for all models which will used. For example, it combines spatial method (interpolation/RBP), domain
     discriminator training, and the DL architecture itself.
     """
+    # ------------
+    # Abstract methods
+    # ------------
+    @abc.abstractmethod
+    def train_model(self, *args, **kwargs) -> Tuple[Histories, ...]:
+        """Method for training the model"""
 
+    # ------------
+    # Model saving and loading
+    # ------------
     @classmethod
     def load_model(cls, name: str, path: Path) -> "MainModuleBase":
         model = torch.load((path / name).with_suffix(".pt"))
