3bd1270c6535932d94cd5c7e304bed53ad864ad5
thomas <thomas.tveitstol@gmail.com>
2025-04-09 16:59:52 +0200

    flake8 fixes

--- src/elecssl/data/datasets/getter.py
+++ src/elecssl/data/datasets/getter.py
@@ -24,7 +24,6 @@ def _extract_positions(*, montage, channel_names):
     return {ch_name: channel_positions[ch_name] for ch_name in channel_names}
 
 
-
 def _get_basic_channel_systems():
     """
     Get some pre-defined basic channel systems
@@ -56,7 +55,7 @@ def _get_basic_channel_systems():
 
     # Make all basic channel systems
     names_19 = ("Fp1", "Fp2", "F3", "F4", "C3", "C4", "P3", "P4", "O1", "O2", "F7", "F8", "T3", "T4", "T5", "T6", "Fz",
-                 "Cz", "Pz")
+                "Cz", "Pz")
     names_32 = ("Fp1", "Fp2", "F3", "F4", "C3", "C4", "P3", "P4", "O1", "O2", "F7", "F8", "T7", "T8", "P7", "P8", "Fz",
                 "Cz", "Pz", "AF3", "AF4", "FC1", "FC2", "CP1", "CP2", "PO3", "PO4", "FC5", "FC6", "CP5", "CP6", "Oz")
     names_64 = ('Fp1', 'Fp2', 'F7', 'F3', 'Fz', 'F4', 'F8', 'FC5', 'FC1', 'FC2', 'FC6', 'T7', 'C3', 'Cz', 'C4', 'T8',
@@ -67,12 +66,12 @@ def _get_basic_channel_systems():
     names_126 = ("Fp1", "Fpz", "Fp2", "F7", "F3", "Fz", "F4", "F8", "FC5", "FC1", "FC2", "FC6", "M1", "T7", "C3", "Cz",
                  "C4", "T8", "M2", "CP5", "CP1", "CP2", "CP6", "P7", "P3", "Pz", "P4", "P8", "POz", "O1", "O2", "AF7",
                  "AF3", "AF4", "AF8", "F5", "F1", "F2", "F6", "FC3", "FCz", "FC4", "C5", "C1", "C2", "C6", "CP3", "CP4",
-                 "P5", "P1", "P2", "P6", "F9", "PO3", "PO4", "F10", "FT7", "FT8", "TP7", "TP8","PO7", "PO8", "FT9",
+                 "P5", "P1", "P2", "P6", "F9", "PO3", "PO4", "F10", "FT7", "FT8", "TP7", "TP8", "PO7", "PO8", "FT9",
                  "FT10", "TPP9h", "TPP10h", "PO9", "PO10", "P9", "P10", "AFF1", "AFz", "AFF2", "FFC5h", "FFC3h",
-                 "FFC4h", "FFC6h", "FCC5h", "FCC3h", "FCC4h", "FCC6h", "CCP5h", "CCP3h", "CCP4h","CCP6h", "CPP5h",
-                 "CPP3h", "CPP4h", "CPP6h", "PPO1", "PPO2", "I1", "Iz", "I2", "AFp3h", "AFp4h","AFF5h", "AFF6h",
+                 "FFC4h", "FFC6h", "FCC5h", "FCC3h", "FCC4h", "FCC6h", "CCP5h", "CCP3h", "CCP4h", "CCP6h", "CPP5h",
+                 "CPP3h", "CPP4h", "CPP6h", "PPO1", "PPO2", "I1", "Iz", "I2", "AFp3h", "AFp4h", "AFF5h", "AFF6h",
                  "FFT7h", "FFC1h", "FFC2h", "FFT8h", "FTT9h", "FTT7h", "FCC1h", "FCC2h", "FTT8h", "FTT10h", "TTP7h",
-                 "CCP1h", "CCP2h", "TTP8h", "TPP7h", "CPP1h", "CPP2h", "TPP8h", "PPO9h","PPO5h", "PPO6h", "PPO10h",
+                 "CCP1h", "CCP2h", "TTP8h", "TPP7h", "CPP1h", "CPP2h", "TPP8h", "PPO9h", "PPO5h", "PPO6h", "PPO10h",
                  "POO9h", "POO3h", "POO4h", "POO10h", "OI1h", "OI2h")  # From AI-Mind
 
     channel_systems = (
