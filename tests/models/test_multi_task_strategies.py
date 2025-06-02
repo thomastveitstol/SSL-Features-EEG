e6c0dfc292981c35f648629f5052c5a5d5043a26
thomas <thomas.tveitstol@gmail.com>
2025-06-02 10:40:08 +0200

    added tests for UncertaintyWeighting

--- tests/models/test_multi_task_strategies.py
+++ tests/models/test_multi_task_strategies.py
@@ -5,7 +5,7 @@ import torch
 from torch import nn, optim
 
 from elecssl.models.losses import get_pytorch_loss_function
-from elecssl.models.multi_task_strategies import PCGrad, GradNorm
+from elecssl.models.multi_task_strategies import PCGrad, GradNorm, UncertaintyWeighting
 
 
 class _Model(nn.Module):
@@ -120,7 +120,7 @@ def test_pcgrad(in_features, loss, learning_rate, device_name):
 
         # Check gradients have been populated
         grads_present = [p.grad is not None for p in model.parameters() if p.requires_grad]
-        assert all(grads_present), "Some parameters are missing gradients after PCGrad backward."
+        assert all(grads_present), "Some parameters are missing gradients after backpropagation."
 
         # Step
         strategy.step()
@@ -320,7 +320,7 @@ def test_grad_norm(alpha, gradnorm_lr, in_features, loss, learning_rate, device_
     # ---------------
     # Training with GradNorm
     # ---------------
-    # PCGrad setup
+    # GradNorm setup
     base_optim = optim.Adam(model.parameters(), lr=learning_rate)
     strategy = GradNorm(base_optim, alpha=alpha, learning_rate=gradnorm_lr)
     for x, pretext_y, downstream_y in zip(x_batches, pretext_y_batches, downstream_y_batches):
@@ -339,7 +339,7 @@ def test_grad_norm(alpha, gradnorm_lr, in_features, loss, learning_rate, device_
 
         # Check gradients have been populated
         grads_present = [p.grad is not None for p in model.parameters() if p.requires_grad]
-        assert all(grads_present), "Some parameters are missing gradients after PCGrad backward."
+        assert all(grads_present), "Some parameters are missing gradients after backpropagation."
 
         # Step
         strategy.step()
@@ -410,7 +410,7 @@ def test_gradnorm_weights_sign_and_sum(alpha, gradnorm_lr, in_features, loss, le
 ])
 def test_gradnorm_preserves_frozen_layers_and_updates_others(alpha, gradnorm_lr, in_features, loss, learning_rate,
                                                              device_name):
-    """Test if PCGrad implementation preserves the frozen layers and updates the others"""
+    """Test if GradNorm implementation preserves the frozen layers and updates the others"""
     batch_size = 10
     num_dummy_epochs = 30
 
@@ -478,7 +478,138 @@ def test_gradnorm_preserves_frozen_layers_and_updates_others(alpha, gradnorm_lr,
 
 
 # ------------
-# Tests for Uncertainty Weighting
+# Tests for UncertaintyWeighting
 # ------------
-def test_uncertainty_weighting():
-    assert False
+@pytest.mark.parametrize("in_features,loss,learning_rate,device_name", [
+    (1, "L1Loss", 1e-5, "cuda"), (12, "MSELoss", 2.3e-3, "cpu"), (10, "L1Loss", 1e1, "cuda"),
+    (1, "L1Loss", 1e-2, "cpu"), (1, "MSELoss", 1e-5, "cpu")
+])
+def test_uncertainty_weighting(in_features, loss, learning_rate, device_name):
+    """Test of the UncertaintyWeighting works for optimisation"""
+    batch_size = 10
+    num_dummy_epochs = 7
+
+    # Skip if device does not exist
+    device = torch.device(device_name)
+    try:
+        torch.tensor([0.0]).to(device)
+    except (AssertionError, RuntimeError, ValueError) as e:
+        pytest.skip(f"Skipping test on {device_name!r}: {str(e)}")
+
+    # ---------------
+    # Fix dummy model, data, and loss
+    # ---------------
+    # Dummy model
+    model = _create_model(in_features, device)
+
+    # Dummy data
+    x_batches = (torch.rand(batch_size, in_features) for _ in range(num_dummy_epochs))
+    pretext_y_batches = (torch.rand(batch_size, 1) for _ in range(num_dummy_epochs))
+    downstream_y_batches = (torch.rand(batch_size, 1) for _ in range(num_dummy_epochs))
+
+    # Loss function
+    criterion = get_pytorch_loss_function(loss).to(device)
+
+    # ---------------
+    # Training with Uncertainty Weighting
+    # ---------------
+    # Uncertainty Weighting setup
+    base_optim = optim.Adam(model.parameters(), lr=learning_rate)
+    strategy = UncertaintyWeighting(base_optim)
+    for x, pretext_y, downstream_y in zip(x_batches, pretext_y_batches, downstream_y_batches):
+        x = x.to(device)
+        pretext_y = pretext_y.to(device)
+        downstream_y = downstream_y.to(device)
+
+        # Compute losses
+        pretext_yhat, downstream_yhat = model(x, pretext_y=pretext_y)
+        loss1 = criterion(pretext_yhat, pretext_y)
+        loss2 = criterion(downstream_yhat, downstream_y)
+
+        # Backward
+        strategy.zero_grad()
+        strategy.backward(losses=[loss1, loss2], model=model)
+
+        # Check gradients have been populated
+        grads_present = [p.grad is not None for p in model.parameters() if p.requires_grad]
+        assert all(grads_present), "Some parameters are missing gradients after backpropagation."
+
+        # Check that log_vars are now in one of the param groups
+        assert any(strategy._log_vars is p for group in base_optim.param_groups for p in group['params']), \
+            "log_vars were not added to the optimizer"
+
+        # Step
+        strategy.step()
+
+
+@pytest.mark.parametrize("in_features,loss,learning_rate,device_name", [
+    (1, "L1Loss", 1e-5, "cuda"), (12, "MSELoss", 2.3e-3, "cpu"), (10, "L1Loss", 1e1, "cuda"),
+    (1, "L1Loss", 1e-2, "cpu"), (1, "MSELoss", 1e-5, "cpu")
+])
+def test_uncertainty_weighting_preserves_frozen_layers_and_updates_others(in_features, loss, learning_rate,
+                                                                          device_name):
+    """Test if UncertaintyWeighting implementation preserves the frozen layers and updates the others"""
+    batch_size = 10
+    num_dummy_epochs = 30
+
+    # Skip if device does not exist
+    device = torch.device(device_name)
+    try:
+        torch.tensor([0.0]).to(device)
+    except (AssertionError, RuntimeError, ValueError) as e:
+        pytest.skip(f"Skipping test on {device_name!r}: {str(e)}")
+
+    # ---------------
+    # Fix dummy model, data, and loss
+    # ---------------
+    # Dummy model
+    model = _create_model(in_features, device)
+
+    # Freeze the first layer
+    model._main_model[0].weight.requires_grad = False
+    model._main_model[0].bias.requires_grad = False
+
+    # Save the original state
+    original_state = copy.deepcopy({k: v.clone() for k, v in model.state_dict().items()})
+
+    # Dummy data
+    x_batches = (torch.rand(batch_size, in_features) for _ in range(num_dummy_epochs))
+    pretext_y_batches = (torch.rand(batch_size, 1) for _ in range(num_dummy_epochs))
+    downstream_y_batches = (torch.rand(batch_size, 1) for _ in range(num_dummy_epochs))
+
+    # Loss function
+    criterion = get_pytorch_loss_function(loss).to(device)
+
+    # ---------------
+    # Training
+    # ---------------
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    strategy = UncertaintyWeighting(optimizer)
+    for x, pretext_y, downstream_y in zip(x_batches, pretext_y_batches, downstream_y_batches):
+        x = x.to(device)
+        pretext_y = pretext_y.to(device)
+        downstream_y = downstream_y.to(device)
+
+        # Compute losses
+        pretext_yhat, downstream_yhat = model(x, pretext_y=pretext_y)
+        loss1 = criterion(pretext_yhat, pretext_y)
+        loss2 = criterion(downstream_yhat, downstream_y)
+
+        strategy.zero_grad()
+        strategy.backward(losses=[loss1, loss2], model=model)
+        strategy.step()
+
+        # Check frozen layer gradients are None
+        assert model._main_model[0].weight.grad is None, "Frozen weight has a gradient!"
+        assert model._main_model[0].bias.grad is None, "Frozen bias has a gradient!"
+
+    # ---------------
+    # Tests
+    # ---------------
+    # Check that frozen parameters are the same, and that non-frozen parameters changed
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            assert not torch.allclose(param.data, original_state[name]), \
+                f"Trainable parameters ({name!r}) were not updated!"
+        else:
+            assert torch.allclose(param.data, original_state[name]), f"Frozen parameters ({name!r}) were updated!"
